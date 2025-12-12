efc21a407c58065b46224667456ec9b913fc23e6
guizadasaul <guizadasaul25@gmail.com>
2025-12-12 02:53:21 -0400

    fix:errores

---
+++ MicroServiceProducts/MicroServiceProducts.Application/Services/IProductService.cs
@@ -0,0 +1,17 @@
+// New file: IProductService
+using System;
+using System.Collections.Generic;
+using MicroServiceProduct.Domain.Models;
+
+namespace MicroServiceProduct.Application.Services
+{
+    public interface IProductService
+    {
+        void Create(Product product);
+        Product? Read(Guid id);
+        void Update(Product product);
+        void Delete(Guid id);
+        List<Product> GetAll();
+    }
+}
+
---
+++ MicroServiceProducts/MicroServiceProducts.Infraestructure/DataBase/DataBaseConnection.cs
@@ -0,0 +1,48 @@
+﻿using MicroServiceProduct.Domain.Services;
+using Npgsql;
+using System.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroServiceProduct.Infraestructure.DataBase
+{
+    public class DataBaseConnection : IDataBase
+    {
+        private static DataBaseConnection? _instance;
+        private static readonly object _padlock = new object();
+        private readonly string _connectionString;
+
+        private DataBaseConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public static DataBaseConnection GetInstance(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException("connectionString must be provided", nameof(connectionString));
+
+            if (_instance == null)
+            {
+                lock (_padlock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new DataBaseConnection(connectionString);
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public DbConnection GetConnection()
+        {
+            var conn = new NpgsqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
